bd291a370bfaadbadd20ee6d4de9a6934b6b9c44
Asrar-Ahammad <asrarahammadshaik@gmail.com>
2023-05-16 09:31:22 +0530

    Practice

--- Notification/app/src/main/java/com/example/notification/MainActivity.java
+++ Notification/app/src/main/java/com/example/notification/MainActivity.java
@@ -1,46 +1,36 @@
 package com.example.notification;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final int NOTIFICATION_ID = 1;
+    private static final String CHANNEL_ID = "my_channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button notification = findViewById(R.id.notificationButton);
-        notification.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showNotification();
-            }
-        });
+        Button notificationButton = findViewById(R.id.notificationButton);
+        notificationButton.setOnClickListener(v -> showNotification());
     }
-    private void showNotification() {
-        Intent intent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+    private void showNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle("My Notification")
                 .setContentText("Hello, this is a notification!")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
+
 }
